5ad86f631ba7493864fbf77e3e265ff28a17a777
Joe Hans <joseph.hans@gmail.com>
2026-01-19 13:02:09 -0500

    Last commit to correct missing chart

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Joe\projects\louisville_crime_stats_2020\venv
+set VIRTUAL_ENV=C:\Users\Joe\Projects\louisville_crime_stats_2020\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
